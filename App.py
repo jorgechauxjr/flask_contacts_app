58bec90ebf3b6244661557c95a7c5d706991269b
Jorge <chauxjorge@hotmail.com>
2021-05-22 15:52:14 -0500

    Implement Edit. Backend finished CRUD(Create Read Update Delete)

--- App.py
+++ App.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
+# Initializations
 app = Flask(__name__)
 
 # Mysql Connection
@@ -14,6 +15,7 @@ app.secret_key = 'mysecretkey'
 
 mysql = MySQL(app)
 
+# Routes
 @app.route('/')
 def Index():
     # cursor allows to execute queries in mysql
@@ -36,9 +38,32 @@ def add_contact():
         flash('Contact Added successfully')
         return redirect(url_for('Index'))
 
-@app.route('/edit')
-def edit_contact():
-    return "Edit contact"
+@app.route('/edit/<id>')
+def get_contact(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts WHERE id = %s', (id))
+    data = cur.fetchall()
+    print(data[0])
+
+    return render_template('edit-contact.html', contact = data[0])
+
+@app.route('/update/<id>', methods=['POST'])
+def update_contact(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cursor = mysql.connection.cursor()
+        cursor.execute("""
+            UPDATE contacts
+            SET fullname = %s,
+                email = %s,
+                phone = %s
+            WHERE id = %s
+        """, (fullname, email,phone,id))
+        mysql.connection.commit()
+        flash('Contact Updated Successfuly')
+        return redirect(url_for('Index'))
 
 @app.route('/delete/<string:id>')
 def delete_contact(id):
@@ -48,5 +73,8 @@ def delete_contact(id):
     flash('Contact Removed Successfully')
     return redirect(url_for('Index'))
 
+# Starting the app
+# The debug true is to restart automatically the server when save a change
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
+ 
